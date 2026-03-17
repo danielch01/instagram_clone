ea1d4f8e4a08e11ef3deb8fe3f7e7a805c2dc789
danielchoi <dhrchoi@gmail.com>
2026-03-17 18:58:30 -0500

    Filled out the CMakeLists.txt, created a build folder for compilation, and tested to see if main.cpp will run.

---
+++ build/CMakeFiles/ig_clone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ig_clone.dir/src/main.cpp.o"
+  "CMakeFiles/ig_clone.dir/src/main.cpp.o.d"
+  "ig_clone"
+  "ig_clone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ig_clone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
